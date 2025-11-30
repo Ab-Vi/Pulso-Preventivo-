df88062892ba439b36af6645fa19bf9caebfdab0
Ab-Vi <abiimeleccvilar@gmail.com>
2025-11-30 01:53:33 -0500

    Update db.js

--- db.js
+++ db.js
@@ -5,8 +5,12 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+   port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
+
 
 
 
@@ -20,5 +24,6 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
+
 
 
