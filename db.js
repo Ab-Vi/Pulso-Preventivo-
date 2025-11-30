ea6ba925eae5ca6014ccf423b4e67cf0e175c944
Ab-Vi <abiimeleccvilar@gmail.com>
2025-11-30 02:46:21 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,21 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createpool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-   port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 3306,
   ssl: {
     rejectUnauthorized: true
   },
-   waitForConnections: true,
+  waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool;
+
 
 
 
